67c40b634c0e318646535cf40711f2b500e90d33
KANTDEV71 <kantmods71@gmail.com>
2021-07-04 23:24:22 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,42 @@
+const help = (pushname,prefix) => {
+return`
+𝐒𝐚𝐥𝐯𝐞 ${pushname} ♥️(a) 
+╔┬━━━━━━━━━━━┈ ❄︎
+││    KNT 𝐁𝐎𝐓       
+││⇢ᴅᴏɴᴏ: 𝐊𝐀𝐍𝐓
+││⇢ɴᴜᴍᴇʀᴏ: *wa.me/5511942347051*
+││⇢ɪɴsᴛᴀ:@kant_mods
+││⇢ᴄᴀɴᴀʟ:↓↓↓
+││→https://youtube.com/channel/UC0lxBpiY0WJTZqgPSQGWlEg 
+││⇢ᴘɪx:71982507697
+││⇢ᴘʀᴇғɪx: ⌈${prefix}⌋
+││⇢𝚒𝚗𝚏𝚘 𝚌𝚘𝚟𝚒𝚍-19: ⌈${prefix}covid19⌋
+││⇢𝚌𝚊𝚜𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚎 𝚎𝚛𝚛𝚘𝚜: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+ ⊢───◣𝐊𝐀𝐍𝐓 𝐁𝐎𝐓◥───✯
+│
+│⇨ *${prefix}menufig*
+│⚠️ menu de criação de stickers
+│⇨ *${prefix}plays*
+│⚠️ plays de musica
+│Downloads de audios do YT
+│⇨ *${prefix}jogos*
+│⚠️ comandos de jogos 
+│para zoar com amigos.
+│⇨ *${prefix}menuadm* 
+│ ⚠️ administração de grupos
+│⇨ *${prefix}menu2*  EM PRODUÇÃO
+│ ⚠️ segundo menu
+│⇨ *${prefix}kant*
+│ ⚠️ comandos que so o dono do bot pode usar 
+│
+╰────⟪𝐊𝐀𝐍𝐓⟫────┈✯
+©     _*Copyright by kantzn*_
+®     _*dominando tudo⚡*_ 
+╘────────────💫`
+
+}
+
+
+
+exports.help = help
---
+++ src/kant.js
@@ -0,0 +1,28 @@
+const kant = (prefix) => {
+	return `
+╭───────────⌁
+│  🧐🥶 _*𝐊𝐀𝐍𝐓*_ 🥶🧐
+╘┬──────────⌁
+┌┤ 🥵 _*𝐊𝐀𝐍𝐓*_🥵
+││
+││  *${prefix}ban* marque a pessoa com @
+││⚠️ banir uma pessoa de usar o bot
+││  *${prefix}unban*
+││⚠️ desbane pessoa banida 
+││  *${prefix}aviso*
+││⚠️ para fazer um aviso
+││  *${prefix}divulgar*
+││⚠️ de aviso de videos
+││  *${prefix}bc*
+││⚠️ fazer tm para  todos os usuários
+││  *${prefix}setprefix*
+││⚠️ mudar o prefixo do bot
+││  *${prefix}hidetag*
+││⚠️ marcação invisível
+││
+│╰──────────┈┈💫
+│             _*𝐊𝐀𝐍𝐓 bot*_
+╰───────────┈┈💫`
+}
+
+exports.kant = kant
